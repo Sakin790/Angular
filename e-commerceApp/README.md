eb1087d798816bfcb886a39c150fd89b5b015b9e
Sakin790 <sakinislam79@gmail.com>
2024-04-23 22:06:34 +0600

    project init

--- e-commerceApp/README.md
+++ e-commerceApp/README.md
@@ -1,4 +1,4 @@
-# MyApp
+# ECommerceApp
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 17.3.5.
 
--- e-commerceApp/src/app/app.component.spec.ts
+++ e-commerceApp/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'myApp' title`, () => {
+  it(`should have the 'e-commerceApp' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('myApp');
+    expect(app.title).toEqual('e-commerceApp');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, myApp');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, e-commerceApp');
   });
 });
