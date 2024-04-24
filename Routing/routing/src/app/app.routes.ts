f72524e04cb4a3f299ce4f112d5a0dedf6f65d0b
Sakin790 <sakinislam79@gmail.com>
2024-04-24 12:54:17 +0600

    all working

--- Routing/routing/src/app/app.routes.ts
+++ Routing/routing/src/app/app.routes.ts
@@ -3,9 +3,11 @@ import { HomeComponent } from '../app/home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 export const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'about', component: AboutComponent },
-  { path: 'contact', component: ContactComponent },
+  { path: '', title: 'Home', component: HomeComponent },
+  { path: 'about', title: 'About', component: AboutComponent },
+  { path: 'contact', title: 'Contact', component: ContactComponent },
   { path: 'navbar', component: NavbarComponent },
+  { path: '**', component: NotFoundComponent },
 ];
