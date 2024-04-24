892c3a2ede1d94b0578e66211bb6e7130ec98126
Sakin790 <sakinislam79@gmail.com>
2024-04-24 09:18:25 +0600

    working

--- e-commerceApp/src/app/app.component.ts
+++ e-commerceApp/src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { HeaderComponent } from '../app/header/header.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,HeaderComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'e-commerceApp';
